f1c4826e9d55633524ebfef47193f7ee1e0662b0
DESKTOP-8PVUKGP\mohsen <mohsen.sanjari@outlook.com>
2018-07-22 13:46:22 +1000

    Added rappid to the project

--- WidgetUI/src/app/app.component.ts
+++ WidgetUI/src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import * as joint from 'jointjs';
+
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,42 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+
+  ngAfterViewInit():void{
+    // var graph = new joint.dia.Graph;
+    var graph = new joint.dia.Graph;
+    // joint.ui.SelectionView
+    var paper = new joint.dia.Paper({
+      el: document.getElementById('myholder'),
+      model: graph,
+      width: 600,
+      height: 100,
+      gridSize: 1
+  });
+
+  var rect = new joint.shapes.standard.Rectangle();
+  rect.position(100, 30);
+  rect.resize(100, 40);
+  rect.attr({
+      body: {
+          fill: 'blue'
+      },
+      label: {
+          text: 'Hello',
+          fill: 'white'
+      }
+  });
+  rect.addTo(graph);
+
+  var rect2 = rect.clone();
+  rect2.translate(300, 0);
+  rect2.attr('label/text', 'World!');
+  rect2.addTo(graph);
+
+  var link = new joint.shapes.standard.Link();
+  link.source(rect);
+  link.target(rect2);
+  link.addTo(graph);
+  }
+
 }
